598b0cbea717b6a245c6fb3c080889d6a2fa1fc8
SNBest1 <nipun.saini9@gmail.com>
2025-11-09 12:22:22 -0800

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/fintech-hackathon-huskyhack/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
